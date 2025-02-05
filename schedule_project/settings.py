b8d48d29629995786d4280e03f6674f37d418b64
Elshaddai Mhango <emhango8486@kvcc.edu>
2025-02-05 14:14:20 -0500

    Revitalized the project after restoring from the previous commit

--- schedule_project/settings.py
+++ schedule_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$9cqwzmpmk51uu!+r#xs&+-9=szabp(gzo^_brox$bk!!26*7l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['elshaddaii.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
